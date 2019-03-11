ebeec2a620a86752e2de7f428b10a9319082b2a9
Joseph Lozier <joseph.lozier@gmail.com>
2019-03-10 20:25:19 -0400

    piece config output

--- lib/Piece.cxx
+++ lib/Piece.cxx
@@ -58,3 +58,38 @@ bool Piece::IsConnected() const
   }
   return true;
 }
+
+std::ostream& operator<<(std::ostream& os, const Piece& p)
+{
+  for (Coordinates::Type z{-5}; z < 6; z += 2) {
+    int plane{
+        p.IsOccupied({1, -1, z})     +
+        p.IsOccupied({1,  1, z}) * 2 +
+        p.IsOccupied({3, -1, z}) * 4 +
+        p.IsOccupied({3,  1, z}) * 8
+    };
+
+    switch (plane) {
+      case  0: os << "0"; break;
+      case  1: os << "T"; break;
+      case  2: os << "L"; break;
+      case  3: os << ":"; break;
+      case  4: os << "M"; break;
+      case  5: os << "^"; break;
+      case  6: os << "?"; break;
+      case  7: os << "?"; break;
+      case  8: os << "W"; break;
+      case  9: os << "?"; break;
+      case 10: os << "V"; break;
+      case 11: os << "?"; break;
+      case 12: os << "|"; break;
+      case 13: os << "?"; break;
+      case 14: os << "?"; break;
+      case 15: os << "-"; break;
+      default: throw std::logic_error("bad plane config");
+    }
+  }
+
+  return os;
+}
+
--- lib/Piece.h
+++ lib/Piece.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <ostream>
+
 #include "Coordinates.h"
 
 class Piece{
@@ -15,3 +17,5 @@ private:
 
   Type mData;
 };
+
+std::ostream& operator<<(std::ostream&, const Piece&);
